3b3289f74dae505087d237334efa435acc74d01c
rishav <rishav.tandon@itera.io>
2021-10-05 00:55:46 +0200

    feat: add ejs templating engine

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
+const path = require('path');
+
 const express = require('express');
-const path = require('path')
 
 const rootDir = require('../utils/path');
 
@@ -7,13 +8,21 @@ const router = express.Router();
 
 const products = [];
 
-router.get('/add-product' ,(req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-router.post('/add-product' ,(req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
 exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,22 @@
-const express = require('express');
 const path = require('path');
 
+const express = require('express');
+
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-const adminData = require('./admin');
-
-router.get('/' ,(req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
