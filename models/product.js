797bf97e1c680e75c0df1e899d4cb62bce708de8
rishav <rishav.tandon@itera.io>
2021-11-08 22:40:09 +0100

    feat(product): add product controller, add product model

---
+++ models/product.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(__dirname, '../', 'data' , 'products.json');
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+    })
+  return products;
+}
+
+const products = [];
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+}
